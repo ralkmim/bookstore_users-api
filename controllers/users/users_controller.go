94e53aa3d0f2f4036f74b02be92c5e6a7f3a71d0
Rafael Alkmim <ralkmimpa@gmail.com>
2022-08-05 15:50:11 +0100

    more implementations

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -5,6 +5,7 @@ import (
 	//"fmt"
 	//"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ralkmim/bookstore_users-api/domain/users"
@@ -45,6 +46,20 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("invalid user id")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, user)
+
 }
  
